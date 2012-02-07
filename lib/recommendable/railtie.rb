5e73f631af08debec17936d67f79419d8e3f882f
David Celis <david@davidcelis.com>
2012-02-06 17:03:24 -0800

    Force eager loading of models (when not in development or caching classes).

--- lib/recommendable/railtie.rb
+++ lib/recommendable/railtie.rb
@@ -2,5 +2,14 @@ module Recommendable
   class Railtie < Rails::Railtie
     ActiveRecord::Base.send :include, Recommendable::ActsAsRecommendedTo
     ActiveRecord::Base.send :include, Recommendable::ActsAsRecommendable
+
+    # Force load models if in a non-development environment and not caching classes
+    config.after_initialize do |app|
+      force_load_models if !Rails.env.development? && !app.config.cache_classes
+    end
+
+    def self.force_load_models
+      Dir["#{ Rails.root }/app/models/**/*.rb"].each { |m| load m }
+    end
   end
 end
