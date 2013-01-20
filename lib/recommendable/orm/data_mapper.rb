0e87c639e254f39d71e7901d35065e993ce8c72d
David Celis <me@davidcel.is>
2013-01-20 12:15:47 -0800

    Fix incompatibilities with DataMapper and Mongoid
    Both DataMapper and Mongoid were attempting to include an undefined
    module, Recommendable::Ratable::InstanceMethods. I had forgotten to add
    this after seeing warnings from ActiveSupport::Concern that
    InstanceMethods modules would no longer be included automatically.
    Mongoid and DataMapper need explicit includes, however, so those modules
    will be defined inline for them.
    
    Fixes #58
    
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/recommendable/orm/data_mapper.rb
+++ lib/recommendable/orm/data_mapper.rb
@@ -2,6 +2,11 @@ require 'dm-core'
 
 DataMapper::Model.append_extensions(Recommendable::Rater::ClassMethods)
 DataMapper::Model.append_extensions(Recommendable::Ratable::ClassMethods)
+
+module Recommendable::Ratable::InstanceMethods
+  def recommendable?() self.class.recommendable? end
+end
+
 DataMapper::Model.append_inclusions(Recommendable::Ratable::InstanceMethods)
 
 Recommendable.configure { |config| config.orm = :data_mapper }
--- lib/recommendable/orm/mongoid.rb
+++ lib/recommendable/orm/mongoid.rb
@@ -2,6 +2,11 @@ require 'mongoid'
 
 Mongoid::Document::ClassMethods.send(:include, Recommendable::Rater::ClassMethods)
 Mongoid::Document::ClassMethods.send(:include, Recommendable::Ratable::ClassMethods)
+
+module Recommendable::Ratable::InstanceMethods
+  def recommendable?() self.class.recommendable? end
+end
+
 Mongoid::Document.send(:include, Recommendable::Ratable::InstanceMethods)
 
 Recommendable.configure { |config| config.orm = :mongoid }
