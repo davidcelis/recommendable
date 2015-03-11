62f6851b3fc59f7410e802a81f94f9f39753abd6
David Celis <me@davidcel.is>
2015-03-11 11:59:56 -0700

    Change limit and offset handling
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/recommendable/rater/recommender.rb
+++ lib/recommendable/rater/recommender.rb
@@ -4,15 +4,15 @@ module Recommendable
       # Get a list of raters that have been found to be the most similar to
       # self. They are sorted by the calculated similarity value.
       #
-      # @param [Fixnum] count the number of users to return (defaults to 10)
+      # @param [Fixnum] limit the number of users to return (defaults to 10)
       # @return [Array] An array of instances of your user class
-      def similar_raters(count = 10, offset = 0)
-        ids = Recommendable.redis.zrevrange(Recommendable::Helpers::RedisKeyMapper.similarity_set_for(id), offset, count - 1)
+      def similar_raters(limit = 10, offset = 0)
+        ids = Recommendable.redis.zrevrange(Recommendable::Helpers::RedisKeyMapper.similarity_set_for(id), 0, -1)
 
         order = ids.map { |id| "`#{Recommendable.config.user_class.table_name}`.`id` = %d DESC" }.join(', ')
         order = self.class.send(:sanitize_sql_for_assignment, [order, *ids])
 
-        Recommendable.query(self.class, ids).order(order)
+        Recommendable.query(self.class, ids).order(order).limit(limit).offset(offset)
       end
 
       private
@@ -20,18 +20,18 @@ module Recommendable
       # Fetch a list of recommendations for a passed class.
       #
       # @param [String, Symbol, Class] klass the class from which to get recommendations
-      # @param [Fixnum] count the number of recommendations to fetch (defaults to 10)
+      # @param [Fixnum] limit the number of recommendations to fetch (defaults to 10)
       # @return [Array] a list of things this person's gonna love
-      def recommended_for(klass, count = 10, offset = 0)
+      def recommended_for(klass, limit = 10, offset = 0)
         recommended_set = Recommendable::Helpers::RedisKeyMapper.recommended_set_for(klass, self.id)
         return Recommendable.query(klass, []) unless rated_anything? && Recommendable.redis.zcard(recommended_set) > 0
 
-        ids = Recommendable.redis.zrevrange(recommended_set, offset, count - 1, :with_scores => true)
+        ids = Recommendable.redis.zrevrange(recommended_set, 0, -1, :with_scores => true)
         ids = ids.select { |id, score| score > 0 }.map { |pair| pair.first }
 
         order = ids.map { |id| "`#{klass.table_name}`.`id` = %d DESC" }.join(', ')
         order = klass.send(:sanitize_sql_for_assignment, [order, *ids])
-        Recommendable.query(klass, ids).order(order)
+        Recommendable.query(klass, ids).order(order).limit(limit).offset(offset)
       end
 
       # Removes an item from a user's set of recommendations
