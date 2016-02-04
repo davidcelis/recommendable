83ef7f5e108468986ededac2d445b4499b7d2854
JonghunYu <yuzong@naver.com>
2016-02-04 17:13:44 +0900

    added lock in sidekiq worker

--- lib/recommendable/workers/sidekiq.rb
+++ lib/recommendable/workers/sidekiq.rb
@@ -6,8 +6,15 @@ module Recommendable
       end
 
       def perform(user_id)
+        lock.lock
         Recommendable::Helpers::Calculations.update_similarities_for(user_id)
         Recommendable::Helpers::Calculations.update_recommendations_for(user_id)
+        lock.unlock
+      end
+
+      private
+      def lock
+        $_lock ||= Mutex.new
       end
     end
   end
