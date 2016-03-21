a2cb70c9974c99658da86ebfc01146c777fcceef
JonghunYu <yuzong@naver.com>
2016-03-21 19:32:06 +0900

    skip if there are same job in the queue

--- lib/recommendable/workers/sidekiq.rb
+++ lib/recommendable/workers/sidekiq.rb
@@ -7,6 +7,14 @@ module Recommendable
 
       def perform(user_id)
         lock.lock
+        queue = ::Sidekiq::Queue.new
+        # skip if there are same job in the queue
+        queue.select {|job| job.klass == self.class.to_s }.each do |job|
+          if job.args[0] == user_id
+            lock.unlock
+            return
+          end
+        end
         Recommendable::Helpers::Calculations.update_similarities_for(user_id)
         Recommendable::Helpers::Calculations.update_recommendations_for(user_id)
         lock.unlock
